ca39c1fd8ed200b6ac7ef8143863b9f7b4d2157e
cstgarcia <ckstgarcia@gmail.com>
2022-04-10 17:38:41 -0600

    add interaction in app through React-useState and useEffect, use of axios pckg Promise based HTTP client, add babel plugin to handle the exeption Runtime and modified styles in Menu and ProductList

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/Header.scss';
-
+import Menu from '@components/Menu';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
+    const[toggle, setToggle]= useState(false);
+    const handleToggle = ()=> {
+        setToggle(!toggle);
+    }
+
     return(
     <nav>
         <img src={menu} alt="menu" className="menu"/>
@@ -34,13 +39,16 @@ const Header = () => {
         </div>
         <div className="navbar-right">
             <ul>
-                <li className="navbar-email">isaacgarcia@gmail.com</li>
+                <li className="navbar-email" onClick={handleToggle}>
+                    isaacgarcia@gmail.com
+                </li>
                 <li className="navbar-shoping-cart">
                     <img src={shoppingCart} alt="shoping cart"/>
                     <div>2</div>
                 </li>
             </ul>
         </div>
+        {toggle && <Menu />}
     </nav>
     );
 }
