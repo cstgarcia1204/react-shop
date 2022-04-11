2bf3631042034b166c6a8f77f6562d687a58536f
cstgarcia <ckstgarcia@gmail.com>
2022-04-10 19:01:42 -0600

    add 'useRef' from React inside the form of Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '@styles/Login.scss';
 
 import saleLogo from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
+    const form = useRef(null);
+    const handleSubmit = (event)=> {
+        event.preventDefault();
+        const formData =  new FormData(form.current);
+        const data = {
+            username: formData.get('email'),
+            password: formData.get('password')
+        };
+        console.log(data);
+    }
     return (
         <div className="Login">
         <div className="Login-continer">
             <img src={saleLogo} alt="logo" className="logo"/>
 
-            <form action="/" className="form">
+            <form action="/" className="form" ref={form}>
                 <label for="email" className="label">Email address</label>
-                <input type="text" id="email" placeholder="cinthiagarcia@example.com" className="input input-email"/>
+                <input type="text" name="email" placeholder="cinthiagarcia@example.com" className="input input-email"/>
                 
                 <label for="password" className="label">Password</label>
-                <input type="password" id="password" placeholder="*********" className="input input-password"/>
+                <input type="password" name="password" placeholder="*********" className="input input-password"/>
 
-                <input type="submit" value="Log in" className="primary-button login-button"/>
+                <button className="primary-button login-button" onClick={handleSubmit} >
+                    Log in
+                </button>
                 <a href="/">Forgot my password</a>
             </form>
 
-            <button className="secondary-button signup-button">Sign up</button>
+            <button className="secondary-button signup-button" >Sign up</button>
         </div>
     </div>
     );
